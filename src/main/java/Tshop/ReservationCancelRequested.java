06bbca9e26d8463c3b0804b179cd4cece597fa4a
jayh36 <jhong0306@naver.com>
2020-09-14 15:32:58 +0900

    first commit

---
+++ src/main/java/Tshop/ReservationCancelRequested.java
@@ -0,0 +1,25 @@
+
+package Tshop;
+
+public class ReservationCancelRequested extends AbstractEvent {
+
+    private Long reservationId;
+    private Long productId;
+
+    public Long getReservationId() {
+        return reservationId;
+    }
+
+    public void setReservationId(Long reservationId) {
+        this.reservationId = reservationId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+}
---
+++ src/main/java/Tshop/ReservationRequested.java
@@ -0,0 +1,37 @@
+package Tshop;
+
+import Tshop.AbstractEvent;
+
+public class ReservationRequested extends AbstractEvent {
+
+    private Long reservationId;
+    private Long productId;
+    private Integer quantity;
+
+    public Long getReservationId() {
+        return reservationId;
+    }
+
+    public void setReservationId(Long reservationId) {
+        this.reservationId = reservationId;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+
+
+}
